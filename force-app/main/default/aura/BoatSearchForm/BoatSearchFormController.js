1b71081046a80c78db93d6e4f763a00d5893c0f1
Pierce C Lauring <pclauring@gmail.com>
2018-12-08 19:51:07 -0500

    added logic for new button and pulled in boattypes on init - create boat event/handler

--- force-app/main/default/aura/BoatSearchForm/BoatSearchFormController.js
+++ force-app/main/default/aura/BoatSearchForm/BoatSearchFormController.js
@@ -1,5 +1,44 @@
 ({
-	myAction : function(component, event, helper) {
-		
+	 // Load boatTypes from Salesforce
+	 doInit: function (component, event, helper) {
+		var createRecordEnabled = $A.get("e.force:createRecord");
+        if(createRecordEnabled){
+            component.set("v.renderNewButton",true);
+        } 
+        // Create the action
+        var action = component.get("c.getBoatTypes");
+        // Add callback behavior for when response is received
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.boatTypes", response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        // Send action off to be executed
+        $A.enqueueAction(action);
+	},
+	
+	createNewBoat: function (component, event, helper) {
+		var createBoat = component.getEvent("createBoat");
+		var boatypeID = component.get("v.selectedBoatType")
+		createBoat.setParams({ "boatType": boatypeID });
+		createBoat.fire();
+
+	},
+	handleCreateBoatEvent: function (component, event, helper) {
+		var boatType = event.getParam("boatType");
+		var createRecordEvent = $A.get("e.force:createRecord");
+		createRecordEvent.setParams({
+			"entityApiName": "Boat__c"
+		});
+		if (!boatTypeId == "") {
+			createNewBoat.setParams({
+				"defaultFieldValues": { 'BoatType__c': boatType }
+			})
+		}
+		createRecordEvent.fire();
 	}
 })
